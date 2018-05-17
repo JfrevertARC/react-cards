a446d6122f28ecd45360926fdcb59e8c5fc668b1
Justin Hopkins <hopkinsju@gmail.com>
2018-05-16 21:01:17 -0400

    init commit

---
+++ src/Hello.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Hello.css'
+
+class Hello extends Component {
+  render() {
+    return (
+      <div>
+        <h1>test</h1>
+        <p>Test Paragraph... lorem</p>
+      </div>
+    );
+  }
+}
+export default Hello;
+// export default as 'Hello';
